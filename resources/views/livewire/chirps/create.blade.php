61f67a4ae01576d5f885cbc2b9dd12d180f508e6
Arterning <ning.huang.ridson@gmail.com>
2024-11-20 20:18:19 +0800

    create chirps

--- resources/views/livewire/chirps/create.blade.php
+++ resources/views/livewire/chirps/create.blade.php
@@ -1,7 +1,17 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{rules, state};
 state(['message' => '']);
+
+rules(['message' => 'required|string|max:255']);
+
+$store = function () {
+    $validated = $this->validate();
+
+    auth()->user()->chirps()->create($validated);
+
+    $this->message = '';
+};
 ?>
 
 <div>
